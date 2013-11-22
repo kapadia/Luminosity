4756ae110e92269e40ff5919980021419ba1f03e
Amit Kapadia <amit@zooniverse.org>
2013-11-22 15:00:51 -0600

    remove dead code

--- app/directive/histogram.js
+++ app/directive/histogram.js
@@ -82,7 +82,7 @@ angular.module('LuminosityApp')
           if (!axis1)
             return;
           
-          // Get data from file
+          // Get data from file and create chart
           WorkspaceService.getColumn(axis1, function(data) {
             
             // Get the min, max and histogram
@@ -127,77 +127,6 @@ angular.module('LuminosityApp')
         
         // Broadcast that chart element is ready
         scope.$emit('chart-ready');
-        // svg.append("g")
-        //     .attr("class", "y axis")
-        //     .call(yAxis)
-        //   .append("text")
-        //     .attr("transform", "rotate(-90)")
-        //     .attr("y", 6)
-        //     .attr("dy", ".71em")
-        //     .style("text-anchor", "end")
-        //     .text("Count");
-        // 
-        // var hasData = false;
-        // 
-        // // Watch changes to axes values
-        // scope.$watch('axes.' + index, function() {
-        //   var axis1 = scope.axes[index].axis1;
-        //   if (!axis1)
-        //     return;
-        //   
-        //   // Get data from file
-        //   WorkspaceService.getColumn(scope.axes[index].axis1, function(data) {
-        //     var extent, h, bar;
-        //     
-        //     // Get the min, max and histogram
-        //     // TODO: Bayesian Blocks?
-        //     extent = WorkspaceService.getExtent(data);
-        //     h = HistogramService.compute(data, extent[0], extent[1], 100);
-        //     
-        //     // Set axes domains and transition
-        //     x.domain(extent);
-        //     y.domain([0, d3.max(h)]);
-        //     svg.select(".x").transition().duration(500).call(xAxis);
-        //     svg.select(".y").transition().duration(500).call(yAxis);
-        //     
-        //     if (hasData) {
-        //       bar = svg.selectAll(".bar")
-        //             .data(h)
-        //           .transition()
-        //             .duration(500)
-        //             .attr("transform", function(d, i) { return "translate(" + x(extent[0] + i * h.dx) + "," + y(d) + ")"; });
-        //       
-        //       bar.select("rect")
-        //         .attr("x", 1)
-        //         .attr("width", x(h.dx) - 1)
-        //         .attr("height", function(d) { return height - y(d); });
-        //     } else {
-        //       bar = svg.selectAll(".bar")
-        //           .data(h)
-        //         .enter().append("g")
-        //           .attr("class", "bar")
-        //           .attr("transform", function(d, i) { return "translate(" + x(extent[0] + i * h.dx) + "," + y(d) + ")"; });
-        //           
-        //       bar.append("rect")
-        //           .attr("x", 1)
-        //           .attr("width", x(h.dx) - 1)
-        //           .attr("height", function(d) { return height - y(d); });
-        //     }
-        //     
-        //     hasData = true;
-        //   })
-        // }, true)
-        // 
-        // // Listen for layout changes (e.g. chart added)
-        // scope.$on('chart-added', function() {
-        //   var width, height;
-        //   
-        //   // With a new chart, the layout has changes and the parent div has new dimensions
-        //   width = element[0].offsetWidth;
-        //   console.log('new width', width, element[0]);
-        //   
-        // });
-        
       }
     };
   });
